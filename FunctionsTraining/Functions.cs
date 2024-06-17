5127eaf1aa91c856a573c6c4ae809aeea45777da
buseanli <elvanbuseanli@gmail.com>
2024-06-17 20:01:06 +0300

    A code for checking palindromes has implemented.

---
+++ FunctionsTraining/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Answer
+{
+    // Change these Boolean values to control whether you see
+    // the expected result and/or hints.
+    public static bool ShowExpectedResult = false;
+    public static bool ShowHints = false;
+
+    // Determine whether a string is a Palindrome
+    public static bool IsPalindrome(string thestr)
+    {
+        // Your code goes here.
+        thestr = thestr.Replace("!", "").Replace(" ", "").Replace(",", "").Replace("'", "").Replace(".", "");
+        thestr = thestr.ToLower();
+
+        for (int j = 0; j < thestr.Length; j++)
+        {
+            if (thestr[j] != thestr[thestr.Length - j - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        string[] teststrings = { "Hello World!", "Race car!", "Rotor", "More cowbell!", "Madam, I'm Adam." };
+        int palcount = 0;
+        foreach (string str in teststrings)
+        {
+            bool learnerResult = Answer.IsPalindrome(str);
+            if (learnerResult)
+                palcount++;
+        }
+    }
+}
