7813c070fbc30718ad9ec2fd4b41f07494d9e975
buseanli <elvanbuseanli@gmail.com>
2024-06-21 10:05:58 +0300

    Object Oriented Programming exercises have been implemented

--- OOP/OOP.cs
+++ OOP/OOP.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace OOP
+{
+    public class Book
+    {
+        string _name;
+        private string _author;
+        private int _pagecount;
+
+        public Book(string name, string author, int pages)
+        {
+            _name = name;
+            _author = author;
+            _pagecount = pages;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => _author = value;
+        }
+
+        public int Pagecount
+        {
+            get => _pagecount;
+            set => _pagecount = value;
+        }
+        public string GetDescription()
+        {
+            return $"{Name} by {Author}, {Pagecount} pages";
+        }
+
+        public string ISBN
+        {
+            get;
+            set;
+        }
+
+        public decimal price
+        {
+            get;
+            set;
+        }
+    }
+}
